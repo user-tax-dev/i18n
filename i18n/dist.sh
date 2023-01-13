9511f34dab127362b699bbf6ef6f1d26ca890c97
usrtax <i@user.tax>
2023-01-13 14:09:37 +0800

    v0.0.53

--- i18n/dist.sh
+++ i18n/dist.sh
@@ -1,27 +1,30 @@
 #!/usr/bin/env bash
 
-_DIR=$(cd "$(dirname "$0")"; pwd)
+_DIR=$(
+  cd "$(dirname "$0")"
+  pwd
+)
 
 cd $_DIR
 
 set -ex
 
-version=$(cat package.json|jq -r '.version')
+version=$(cat package.json | jq -r '.version')
 
 ./build.sh &
 mdi &
 wait
 
-add(){
-git add -u
-git commit -m v$version || true
+add() {
+  git add -u
+  git commit -m v$version || true
 }
 
 add
 
 git pull
 
-npm set unsafe-perm true
+#npm set unsafe-perm true
 npm version patch
 add
 git push
